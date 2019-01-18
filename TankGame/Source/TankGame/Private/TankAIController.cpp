076f6a3e652c2ee6a8dd810a1049b35f405956da
luklew <luklew10@gmail.com>
2019-01-18 18:01:42 +0000

    AI Controller implementation started.

---
+++ TankGame/Source/TankGame/Private/TankAIController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("AIController Begin Play"));
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController not possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController possessing = %s"), *(ControlledTank->GetName()));
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
---
+++ TankGame/Source/TankGame/Public/TankAIController.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Tank.h"
+#include "TankAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKGAME_API ATankAIController : public AAIController
+{
+	GENERATED_BODY()
+public:
+	ATank* GetControlledTank() const;
+
+	virtual void BeginPlay() override;
+	
+};
