5cc1a89d99f847d4ac440311fd843eb753a15bec
luklew <luklew10@gmail.com>
2019-02-11 16:11:03 +0000

    Projectile Launches from the barrel! (STATIC MESH UE4 POSTCOMPILE BUG STILL PRESENT NEED FIX)

--- TankGame/Source/TankGame/Private/Projectile.cpp
+++ TankGame/Source/TankGame/Private/Projectile.cpp
@@ -8,6 +8,8 @@ AProjectile::AProjectile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement"));
+	ProjectileMovement->bAutoActivate = false;
 }
 
 // Called when the game starts or when spawned
@@ -24,3 +26,9 @@ void AProjectile::Tick(float DeltaTime)
 
 }
 
+void AProjectile::LaunchProjectile(float Speed)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Projectile Fires at %f"), Speed);
+	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
+	ProjectileMovement->Activate();
+}
--- TankGame/Source/TankGame/Public/Projectile.h
+++ TankGame/Source/TankGame/Public/Projectile.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 #include "Projectile.generated.h"
 
 UCLASS()
@@ -15,12 +16,17 @@ public:
 	// Sets default values for this actor's properties
 	AProjectile();
 
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void LaunchProjectile(float Speed);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
+	UProjectileMovementComponent* ProjectileMovement = nullptr;
+
 
 };
